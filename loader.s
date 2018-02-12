f17270aeda29ec3b291049fc3806ded3412aa473
Neeraj Jaiswal <neerajj85@gmail.com>
2018-02-12 23:35:47 +0530

    first time booted

--- loader.s
+++ loader.s
@@ -1,13 +1,15 @@
-global loader ; the entry symbol for ELF
+global loader 
 MAGIC_NUMBER equ 0x1BADB002
 FLAGS equ 0x0
 CHECKSUM equ -MAGIC_NUMBER 
+
 section .text:
 align 4
-dd MAGIC_NUMBER
-dd FLAGS 
-dd CHECKSUM
+    dd MAGIC_NUMBER
+    dd FLAGS 
+    dd CHECKSUM
+
 loader:
-mov eax, 0xCAFEBABE
+    mov eax, 0xCAFEBABE
 .loop:
-jmp .loop
+    jmp .loop
