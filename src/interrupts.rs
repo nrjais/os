1129a73aac0ab7f5764b9095a6512a2018aac841
Neeraj Jaiswal <neerajj85@gmail.com>
2019-12-30 16:01:11 +0530

    Add frame allocator

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -16,6 +16,7 @@ lazy_static! {
   static ref IDT: InterruptDescriptorTable = {
     let mut idt = InterruptDescriptorTable::new();
     idt.breakpoint.set_handler_fn(breakpoint_handler);
+    idt.page_fault.set_handler_fn(page_fault_handler);
     unsafe {
       idt
         .double_fault
@@ -38,6 +39,19 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFra
 
 extern "x86-interrupt" fn double_fault_handler(stack_frame: &mut InterruptStackFrame, _error_code: u64) -> ! {
   panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+use crate::hlt_loop;
+use x86_64::structures::idt::PageFaultErrorCode;
+
+extern "x86-interrupt" fn page_fault_handler(stack_frame: &mut InterruptStackFrame, error_code: PageFaultErrorCode) {
+  use x86_64::registers::control::Cr2;
+
+  println!("EXCEPTION: PAGE FAULT");
+  println!("Accessed Address: {:?}", Cr2::read());
+  println!("Error Code: {:?}", error_code);
+  println!("{:#?}", stack_frame);
+  hlt_loop();
 }
 
 #[derive(Debug, Clone, Copy)]
---
+++ src/memory.rs
@@ -0,0 +1,59 @@
+use bootloader::bootinfo::MemoryMap;
+use bootloader::bootinfo::MemoryRegionType;
+use x86_64::structures::paging::OffsetPageTable;
+use x86_64::{structures::paging::PageTable, VirtAddr};
+use x86_64::{
+  structures::paging::{FrameAllocator, PhysFrame, Size4KiB, UnusedPhysFrame},
+  PhysAddr,
+};
+
+pub struct BootInfoFrameAllocator {
+  memory_map: &'static MemoryMap,
+  next:       usize,
+}
+impl BootInfoFrameAllocator {
+  pub unsafe fn init(memory_map: &'static MemoryMap) -> Self {
+    BootInfoFrameAllocator { memory_map, next: 0 }
+  }
+}
+
+impl BootInfoFrameAllocator {
+  fn usable_frames(&self) -> impl Iterator<Item = UnusedPhysFrame> {
+    // get usable regions from memory map
+    let regions = self.memory_map.iter();
+    let usable_regions = regions.filter(|r| r.region_type == MemoryRegionType::Usable);
+    // map each region to its address range
+    let addr_ranges = usable_regions.map(|r| r.range.start_addr()..r.range.end_addr());
+    // transform to an iterator of frame start addresses
+    let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+    // create `PhysFrame` types from the start addresses
+    let frames = frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)));
+    // we know that the frames are really unused
+    frames.map(|f| unsafe { UnusedPhysFrame::new(f) })
+  }
+}
+
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+  fn allocate_frame(&mut self) -> Option<UnusedPhysFrame> {
+    let frame = self.usable_frames().nth(self.next);
+    self.next += 1;
+    frame
+  }
+}
+
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+  let level_4_table = active_level_4_table(physical_memory_offset);
+  OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
+
+unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
+  use x86_64::registers::control::Cr3;
+
+  let (level_4_table_frame, _) = Cr3::read();
+
+  let phys = level_4_table_frame.start_address();
+  let virt = physical_memory_offset + phys.as_u64();
+  let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
+
+  &mut *page_table_ptr // unsafe
+}
